cfdfb23098c918f352393deeefc1a65237af7f42
jiangvicc <binbinbb520@gmail.com>
2022-03-01 19:26:33 +0800

    add dashboard login check

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>Dashboard</template>
+
+<script>
+export default {
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)jiangsToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    console.log(token);
+    this.$http.defaults.headers.common.Authorization = token;
+    const api = `${process.env.VUE_APP_API}/api/user/check`;
+    this.$http
+      .post(api, this.user)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.dir(error.response.data.message);
+        this.$router.push('login');
+      });
+  },
+};
+</script>
